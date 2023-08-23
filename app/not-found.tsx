ceefba1857bb0d1f96816b9433002d7df28f8c7e
Sourabh Singh Negi <sourabhn376@gmail.com>
2023-08-23 10:48:22 +0530

    added a custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+    return (
+        <div className="text-black">
+            A custom 404 page need to be built here
+        </div>
+    );
+}
+
+export default PageNotFound;
